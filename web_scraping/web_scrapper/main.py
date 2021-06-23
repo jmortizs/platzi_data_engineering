1f3157698be278f99eb0a62795efc9f55fef0ee0
jmortiz <juanmnl.ortiz@gmail.com>
2021-06-22 19:58:52 -0500

    Update to class 20

--- web_scraping/web_scrapper/main.py
+++ web_scraping/web_scrapper/main.py
@@ -1,20 +1,80 @@
-import argparse
+import re
+import csv
 import logging
 logging.basicConfig(level=logging.INFO)
 
+import argparse
+import datetime
 import news_page_objects as news
 
 from common import config
+from requests.exceptions import HTTPError
+from urllib3.exceptions import MaxRetryError
 
 logger = logging.getLogger(__name__)
+is_welL_formed_link = re.compile(r'^https?://.+/.+$')
+is_root_path = re.compile(r'^/.+$')
+
 
 def _news_scraper(news_site_uid):
     host = config()['news_sites'][news_site_uid]['url']
     logging.info(f' Beginnig scraper for {host}')
     homepage = news.HomePage(news_site_uid, host)
 
+    articles = []
     for link in homepage.article_links:
-        print(link)
+        article = _fetch_article(news_site_uid, host, link)
+
+        if article:
+            logger.info(' Article fetched.')
+            articles.append(article)
+
+    _save_articles(news_site_uid, articles)
+
+
+def _save_articles(news_site_uid, articles):
+    now = datetime.datetime.now().strftime('%Y_%m_%d')
+    out_file_name = f'{news_site_uid}_{now}_articles.csv'
+
+    csv_headers = filter(lambda property: not property.startswith('_'), dir(articles[0]))
+    csv_headers = list(csv_headers)
+
+    with open(out_file_name, 'w+') as f:
+        writer = csv.writer(f)
+        writer.writerow(csv_headers)
+
+        for article in articles:
+            row = [str(getattr(article, prop)) for prop in csv_headers]
+            writer.writerow(row)
+
+
+def _fetch_article(news_site_uid, host, link):
+    logger.info(f' Start fetching article at {link}')
+
+    article = None
+    try:
+        article = news.ArticlePage(news_site_uid, _build_link(host, link))
+    except (HTTPError, MaxRetryError) as e:
+        logger.warning(' Error while fetching article', exc_info=False)
+
+    if article and not article.summary:
+        logger.warning(' Invalid article. There is no summary')
+        return None
+
+    return article
+
+
+def _build_link(host, link):
+
+    if is_welL_formed_link.match(link):
+        return link
+
+    elif is_root_path.match(link):
+        return f'{host}{link}'
+
+    else:
+        return f'{host}/{link}'
+
 
 if __name__ == '__main__':
 
@@ -27,5 +87,3 @@ if __name__ == '__main__':
     ARGS = parser.parse_args()
 
     _news_scraper(ARGS.news_site)
-
-    pass
--- web_scraping/web_scrapper/news_page_objects.py
+++ web_scraping/web_scrapper/news_page_objects.py
@@ -2,25 +2,16 @@ import requests
 import bs4
 from common import config
 
-class HomePage:
 
-    def __init__(self, news_site_uid, url):
+class NewsPage:
+
+    def __init__(self, news_site_uid, url) -> None:
         self._url = url
         self._config = config()['news_sites'][news_site_uid]
         self._queries = self._config['queries']
         self._html = None
 
         self._visit(url)
-
-    @property
-    def article_links(self):
-        link_list = []
-        for link in self._select(self._queries['homepage_article_links']):
-            if link and link.has_attr('href'):
-                link_list.append(link)
-
-        return set([self._url + link['href'] for link in link_list])
-
 
     def _select(self, query_string):
         return self._html.select(query_string)
@@ -32,3 +23,37 @@ class HomePage:
         response.encoding = 'utf-8'
 
         self._html = bs4.BeautifulSoup(response.text, 'html.parser')
+
+
+class HomePage(NewsPage):
+
+    def __init__(self, news_site_uid, url):
+        super().__init__(news_site_uid, url)
+
+    @property
+    def article_links(self):
+        link_list = []
+        for link in self._select(self._queries['homepage_article_links']):
+            if link and link.has_attr('href'):
+                link_list.append(link)
+
+        return set([self._url + link['href'] for link in link_list])
+
+
+class ArticlePage(NewsPage):
+
+    def __init__(self, news_site_uid, url) -> None:
+        super().__init__(news_site_uid, url)
+
+    @property
+    def title(self):
+        result = self._select(self._queries['article_title'])
+
+        return result[0].text if len(result) else ''
+
+    @property
+    def summary(self):
+        result = self._select(self._queries['article_summary'])
+
+        return result[0].text if len(result) else ''
+
