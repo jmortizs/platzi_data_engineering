6b671ced452d8794d9531f7d686158435593d4b4
jmortiz <juanmnl.ortiz@gmail.com>
2021-07-02 17:07:12 -0500

    Update to class 31. Added environment.yml

--- web_scraping/web_scrapper/newspaper_recipe.py
+++ web_scraping/web_scrapper/newspaper_recipe.py
@@ -3,9 +3,12 @@ import logging
 logging.basicConfig(level=logging.INFO)
 from urllib.parse import urlparse
 import pandas as pd
+import hashlib
+import nltk
+from nltk.corpus import stopwords
 
 logger = logging.getLogger(__name__)
-
+stop_words = set(stopwords.words('spanish'))
 
 def _read_data(filename):
     logger.info(f'Reading file {filename}')
@@ -33,6 +36,39 @@ def _fill_missing_titles(df):
     return df
 
 
+def _generate_rows_uids(df):
+
+    logger.info('Generating uids for rows')
+    uids = (df
+            .apply(lambda row: hashlib.md5(bytes(row['url'].encode())), axis=1)
+            .apply(lambda hash_object: hash_object.hexdigest()))
+
+    df['uid'] = uids
+
+    return df.set_index('uid')
+
+
+def _remove_new_lines(df):
+    logger.info('Removing new lines from summary')
+
+    stripped_summary = df.apply(lambda row: row['summary'].replace('\n', ''), axis=1)
+    print(stripped_summary)
+    df['summary'] = stripped_summary
+
+    return df
+
+
+def _tokenize_column(df, column_name):
+    return(df
+                .dropna()
+                .apply(lambda row: nltk.word_tokenize(row[column_name]), axis = 1)
+                .apply(lambda tokens: list(filter(lambda token: token.isalpha(), tokens)))
+                .apply(lambda tokens: list(map(lambda token: token.lower(), tokens)))
+                .apply(lambda word_list: list(filter(lambda word: word not in stop_words, word_list)))
+                .apply(lambda valid_word_list: len(valid_word_list))
+          )
+
+
 def main(filename):
     logger.info('Starting cleaning process')
 
@@ -43,6 +79,10 @@ def main(filename):
     df['newspaper'] = newspaper_uid
     df = _get_host(df)
     df = _fill_missing_titles(df)
+    df = _generate_rows_uids(df)
+    df = _remove_new_lines(df)
+    df['n_tokens_title'] = _tokenize_column(df, 'title')
+    df['n_tokens_summary'] = _tokenize_column(df, 'summary')
 
     return df
 
